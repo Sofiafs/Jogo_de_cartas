fc28a1cae5e7a0a01e64301426b4863d366da2ef
ALUNO SESI PAULISTA <aluno.sesipaulista@SISTEMAFIEPE.LOCAL>
2024-03-21 17:47:24 -0300

    primeiro commit da classe bararlho

---
+++ baralho.py
@@ -0,0 +1,19 @@
+import random
+
+class Baralho:
+    def __init__(self):
+        naipes = ['Paus', 'Ouros', 'Copas', 'Espadas']
+        valores = ['Ás', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Valete', 'Dama', 'Rei']
+        self.cartas = [{'valor': v, 'naipe': n} for n in naipes for v in valores]
+    def embaralhar(self):
+        random.shuffle(self.cartas)
+    
+    def distribuir_carta(self):
+        if len(self.cartas) == 0:
+            return None
+        return self.cartas.pop()
+    
+b01 = Baralho()
+b01.embaralhar()
+cd01 = Baralho.distribuir_carta(b01)
+print(cd01)
